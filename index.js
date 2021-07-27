2697d816357038b6bef5a8605b12aef54dfea121
Jose <acevedosharp@hotmail.com>
2021-07-27 10:11:41 -0500

    changes supporting folder types, sizes and choose image format conditionally

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const AWS = require('aws-sdk');
 
 exports.handler = async (event) => {
 
+    const perFolderSizeMappings = {
+        "product-images": { width: 210, height: 210 },
+        "category-images": { width: 80, height: 80 },
+        "promotion-images": { width: 477, height: 267 },
+        "banner": { width: 1624, height: 400 }
+    }
+
     const S3 = new AWS.S3({
         signatureVersion: 'v4',
         region: 'us-east-2',
@@ -16,6 +23,12 @@ exports.handler = async (event) => {
     for (const record of event.Records) {
 
         const objectKey = record.s3.object.key
+        const dotIndex = objectKey.lastIndexOf('.')
+        const objectKeyWithoutExtension = objectKey.substring(0, dotIndex)
+        const objectExtension = objectKey.substring(dotIndex+1, objectKey.lenth)
+        const targetExtension = ['jpeg', 'jpg'].includes(objectExtension) ? 'jpeg' : 'webp'
+
+        const targetFolder = objectKey.split['/']
 
         try {
             const data = await S3.getObject({
@@ -23,16 +36,32 @@ exports.handler = async (event) => {
                 Key: objectKey
             }).promise()
 
-            const buffer = await sharp(data.Body)
-                .resize(100, 100, { fit: sharp.fit.contain })
-                .webp({ quality: 100 })
-                .toBuffer()
+            let buffer
+            if (targetExtension === 'jpeg') {
+                buffer = await sharp(data.Body)
+                    .resize({
+                        width: perFolderSizeMappings[targetFolder].width,
+                        height: perFolderSizeMappings[targetFolder].height,
+                        fit: sharp.fit.cover
+                    })
+                    .jpeg({ quality: 80, mozjpeg: true })
+                    .toBuffer()
+            } else {
+                buffer = await sharp(data.Body)
+                    .resize({
+                        width: perFolderSizeMappings[targetFolder].width,
+                        height: perFolderSizeMappings[targetFolder].height,
+                        fit: sharp.fit.cover
+                    })
+                    .webp(targetFolder === 'banner' ? { quality: 100, lossless: true } : { quality: 75 })
+                    .toBuffer()
+            }
 
             await S3.putObject({
-                Key: objectKey,
+                Key: `${objectKeyWithoutExtension}.${targetExtension}`,
                 Body: buffer,
                 Bucket: 'merca4-images',
-                ContentType: 'image/webp',
+                ContentType: `image/${targetExtension}`,
                 ACL: 'public-read'
             }).promise()
         } catch (e) {
